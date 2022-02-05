08974d85594b0d50d6db1d4cd10b67f312760eb3
Anand Nigam <anandnigam.2aug@gmail.com>
2022-02-05 23:02:20 +0530

    Initial Commit

---
+++ ConfigTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConfigTest
+//
+//  Created by Anand Nigam on 05/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
