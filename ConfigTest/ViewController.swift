c2c5f792a2bb3a0cd645011bc00ba386ee39e5af
Anand Nigam <anandnigam.2aug@gmail.com>
2022-05-23 18:03:37 +0530

    complete project in place

--- ConfigTest/ViewController.swift
+++ ConfigTest/ViewController.swift
@@ -13,7 +13,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        showConfigAlert()
+    }
 
-
+    func showConfigAlert() {
+        let alert = UIAlertController(title: "Config Values", message: "Base URL - \(Config.baseURL)", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .cancel))
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
